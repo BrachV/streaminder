cb7c783c75993c6e618a8d0f680c8472fcdab784
BrachV <v.brach57@gmail.com>
2023-12-08 21:25:03 +0100

    feat(front): home page linear style

--- locale.js
+++ locale.js
@@ -1,3 +1,6 @@
 document.querySelectorAll("[data-locale]").forEach((elem) => {
   elem.innerText = chrome.i18n.getMessage(elem.dataset.locale);
 });
+document.querySelectorAll("[data-locale-placeholder]").forEach((input) => {
+	input.placeholder = chrome.i18n.getMessage(input.dataset.localePlaceholder);
+  });
